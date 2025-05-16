51725205a6af248f6ca9561bef88d8f2b59695ee
Ben Taussig <btaussig97@gmail.com>
2025-05-16 15:50:25 -0700

    orchestrate battle with a state machine

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -4,56 +4,75 @@ var turn: int = 0
 var turn_order_index: int = 0
 var battle_participants = []
 
+enum State {WAITING_FOR_INPUT, PLAYER_ATTACK_INFO, ENEMY_ATTACK, ENEMY_ATTACK_INFO}
+var state: State = State.WAITING_FOR_INPUT
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_init_battle_participants()
+	_render_hp()
+
+	for i in %Player.moves.size():
+		%Attack.get_child(i).text = %Player.moves[i].move_name
+
+	if battle_participants[turn_order_index].is_player:
+		_update_state(State.WAITING_FOR_INPUT)
+	else:
+		_update_state(State.ENEMY_ATTACK)
+
+func _update_state(new_state: State):
+	var old_state = state
+	state = new_state
+
+	if old_state == State.WAITING_FOR_INPUT:
+		%Action.visible = true
+		%Attack.visible = false
+	if old_state == State.PLAYER_ATTACK_INFO || old_state == State.ENEMY_ATTACK_INFO:
+		_render_hp()
+
+	if state == State.WAITING_FOR_INPUT:
+		%Status.size.x = 255
+		%Status.text = "What will PLAYER do"
+		%Action.visible = true
+		%Attack.visible = false
+		%Action.get_child(0).grab_focus()
+	elif state == State.ENEMY_ATTACK:
+		%Enemy.use_move(0, %Player)
+		_update_state(State.ENEMY_ATTACK_INFO)
+	elif state == State.PLAYER_ATTACK_INFO:
+		%Action.visible = false
+		%Status.size.x = 255
+		%Status.text = "Player Attacked Enemy"
+	elif state == State.ENEMY_ATTACK_INFO:
+		%Action.visible = false
+		%Status.size.x = 255
+		%Status.text = "Enemy Attacked Player"
+
+func _init_battle_participants():
 	for enemy in %Enemies.get_children():
 		enemy.speed = 20
 		enemy.is_player = false
 		battle_participants.append(enemy)
 	battle_participants.append(%Player)
 	battle_participants.sort_custom(func(a, b): return a.speed - b.speed)
-	_render_hp()
-	_start_battle()
-	for i in %Player.moves.size():
-		%Attack.get_child(i).text = %Player.moves[i].move_name
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-func _render_hp() -> void:
-	%EnemyPanel.text = "Enemy " + str(%Enemy.hp) + " / " + str(%Enemy.max_hp)
-	%PlayerPanel.text = "Enemy " + str(%Player.hp) + " / " + str(%Player.max_hp)
-	pass
-	
-func _start_battle():
-	if battle_participants[turn_order_index].is_player:
-		%Action.get_child(0).grab_focus()
-	else:
-		_enemy_turn()
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		if state == State.PLAYER_ATTACK_INFO:
+			_update_state(State.ENEMY_ATTACK)
+		elif state == State.ENEMY_ATTACK_INFO:
+			_update_state(State.WAITING_FOR_INPUT)
+
 func _on_attack_pressed() -> void:
 	%Action.visible = false
 	%Attack.visible = true
 	%Attack.get_child(0).grab_focus()
-	
+
 func _on_move_pressed(index: int) -> void:
 	%Player.use_move(index, %Enemy)
-	_increment_turn()
+	_update_state(State.PLAYER_ATTACK_INFO)
 
-func _increment_turn():
-	%Action.visible = true
-	%Attack.visible = false
-	_render_hp()
-	turn_order_index += 1
-	if turn_order_index == battle_participants.size():
-		turn_order_index = 0
-		turn += 1
-	if battle_participants[turn_order_index].is_player:
-		%Action.get_child(0).grab_focus()
-	else:
-		_enemy_turn()
-		
-func _enemy_turn():
-	%Enemy.use_move(0, %Player)
-	_increment_turn()
-	
+func _render_hp() -> void:
+	%EnemyPanel.text = "Enemy " + str(%Enemy.hp) + " / " + str(%Enemy.max_hp)
+	%PlayerPanel.text = "Enemy " + str(%Player.hp) + " / " + str(%Player.max_hp)
+	pass
