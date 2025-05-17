cdd40ceaa5faa6cff8f059608c4e70a756f44467
Ben Taussig <btaussig97@gmail.com>
2025-05-17 10:57:18 -0700

    separate `WAITING_FOR_INPUT` into `SELECTING_ACTION` and `SELECTING_ATTACK`

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -4,8 +4,8 @@ var turn: int = 0
 var turn_order_index: int = 0
 var battle_participants = []
 
-enum State {WAITING_FOR_INPUT, PLAYER_ATTACK_INFO, ENEMY_ATTACK, ENEMY_ATTACK_INFO}
-var state: State = State.WAITING_FOR_INPUT
+enum State {SELECTING_ACTION, SELECTING_ATTACK, PLAYER_ATTACK_INFO, ENEMY_ATTACK, ENEMY_ATTACK_INFO}
+var state: State = State.SELECTING_ACTION
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +16,7 @@ func _ready() -> void:
 		%Attack.get_child(i).text = %Player.moves[i].move_name
 
 	if battle_participants[turn_order_index].is_player:
-		_update_state(State.WAITING_FOR_INPUT)
+		_update_state(State.SELECTING_ACTION)
 	else:
 		_update_state(State.ENEMY_ATTACK)
 
@@ -26,29 +26,33 @@ func _update_state(new_state: State):
 
 	%StateDisplay.text = State.keys()[state]
 
-	if old_state == State.WAITING_FOR_INPUT:
-		%Action.visible = true
+	if old_state == State.SELECTING_ACTION:
+		%Action.visible = false
+	elif old_state == State.SELECTING_ATTACK:
 		%Attack.visible = false
-	if old_state == State.PLAYER_ATTACK_INFO || old_state == State.ENEMY_ATTACK_INFO:
+	elif old_state == State.PLAYER_ATTACK_INFO || old_state == State.ENEMY_ATTACK_INFO:
+		%ContinueButton.visible = false
 		_render_hp()
 
-	if state == State.WAITING_FOR_INPUT:
+	if state == State.SELECTING_ACTION:
 		%Status.size.x = 255
-		%Status.text = "What will PLAYER do"
+		%Status.text = "What will PLAYER do?"
 		%Action.visible = true
-		%Attack.visible = false
 		%Action.get_child(0).grab_focus()
+	elif state == State.SELECTING_ATTACK:
+		%Attack.visible = true
+		%Attack.get_child(0).grab_focus()
 	elif state == State.ENEMY_ATTACK:
 		%Enemy.use_move(0, %Player)
 		_update_state(State.ENEMY_ATTACK_INFO)
 	elif state == State.PLAYER_ATTACK_INFO:
-		%Action.visible = false
-		%Status.size.x = 255
 		%Status.text = "Player Attacked Enemy"
+		%ContinueButton.visible = true
+		%ContinueButton.grab_focus()
 	elif state == State.ENEMY_ATTACK_INFO:
-		%Action.visible = false
-		%Status.size.x = 255
 		%Status.text = "Enemy Attacked Player"
+		%ContinueButton.visible = true
+		%ContinueButton.grab_focus()
 
 func _init_battle_participants():
 	for enemy in %Enemies.get_children():
@@ -58,23 +62,20 @@ func _init_battle_participants():
 	battle_participants.append(%Player)
 	battle_participants.sort_custom(func(a, b): return a.speed - b.speed)
 
-func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
-		if state == State.PLAYER_ATTACK_INFO:
-			_update_state(State.ENEMY_ATTACK)
-		elif state == State.ENEMY_ATTACK_INFO:
-			_update_state(State.WAITING_FOR_INPUT)
-
-func _on_attack_pressed() -> void:
-	%Action.visible = false
-	%Attack.visible = true
-	%Attack.get_child(0).grab_focus()
-
 func _on_move_pressed(index: int) -> void:
 	%Player.use_move(index, %Enemy)
 	_update_state(State.PLAYER_ATTACK_INFO)
 
 func _render_hp() -> void:
 	%EnemyPanel.text = "Enemy " + str(%Enemy.hp) + " / " + str(%Enemy.max_hp)
-	%PlayerPanel.text = "Enemy " + str(%Player.hp) + " / " + str(%Player.max_hp)
-	pass
+	%PlayerPanel.text = "Player " + str(%Player.hp) + " / " + str(%Player.max_hp)
+
+func _on_info_button_pressed() -> void:
+	if state == State.PLAYER_ATTACK_INFO:
+		_update_state(State.ENEMY_ATTACK)
+	elif state == State.ENEMY_ATTACK_INFO:
+		_update_state(State.SELECTING_ACTION)
+
+func _on_attack_pressed() -> void:
+	if state == State.SELECTING_ACTION:
+		_update_state(State.SELECTING_ATTACK)
