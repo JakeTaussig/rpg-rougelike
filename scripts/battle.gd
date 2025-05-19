e52738e67373d80ea7addb2355edd5a1aa7c2fd1
Ben Taussig <btaussig97@gmail.com>
2025-05-19 13:49:44 -0700

    combine `PLAYER_WIN` and `PLAYER_LOSS` states into `GAME_END`

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -4,7 +4,7 @@ var turn: int = 0
 var turn_order_index: int = 0
 var battle_participants = []
 
-enum State {SELECTING_ACTION, SELECTING_ATTACK, PLAYER_ATTACK_INFO, ENEMY_ATTACK, ENEMY_ATTACK_INFO, PLAYER_WIN, PLAYER_LOSS}
+enum State {SELECTING_ACTION, SELECTING_ATTACK, PLAYER_ATTACK_INFO, ENEMY_ATTACK, ENEMY_ATTACK_INFO, GAME_END}
 var state: State = State.SELECTING_ACTION
 
 # Called when the node enters the scene tree for the first time.
@@ -35,15 +35,16 @@ func _update_state(new_state: State):
 		%BattleStatus.visible = false
 		_render_hp()
 		if %Enemy.hp <= 0:
-			_update_state(State.PLAYER_WIN)
+			%BattleStatus.text = "Player defeated Enemy"
+			_update_state(State.GAME_END)
 			return
 		elif %Player.hp <= 0:
-			_update_state(State.PLAYER_LOSS)
+			%BattleStatus.text = "Enemy defeated Player"
+			_update_state(State.GAME_END)
 			return
 
 	if state == State.SELECTING_ACTION:
 		%PlayerPrompt.visible = true
-		#%PlayerPrompt.size.x = 255
 		%PlayerPrompt.text = "What will PLAYER do?"
 		%Action.visible = true
 		%Action.get_child(0).grab_focus()
@@ -61,13 +62,8 @@ func _update_state(new_state: State):
 		%BattleStatus.visible = true
 		%BattleStatus.text = "Enemy Attacked Player"
 		%ContinueButton.grab_focus()
-	elif state == State.PLAYER_WIN:
+	elif state == State.GAME_END:
 		%BattleStatus.visible = true
-		%BattleStatus.text = "Player defeated Enemy"
-		%ContinueButton.grab_focus()
-	elif state == State.PLAYER_LOSS:
-		%BattleStatus.visible = true
-		%BattleStatus.text = "Enemy defeated Player"
 		%ContinueButton.grab_focus()
 
 func _init_battle_participants():
@@ -91,7 +87,7 @@ func _on_continue_button_pressed() -> void:
 		_update_state(State.ENEMY_ATTACK)
 	elif state == State.ENEMY_ATTACK_INFO:
 		_update_state(State.SELECTING_ACTION)
-	elif state == State.PLAYER_WIN || state == State.PLAYER_LOSS:
+	elif state == State.GAME_END:
 		get_tree().quit()
 
 func _on_attack_pressed() -> void:
