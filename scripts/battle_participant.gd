74faaf68c8c9921ede871b8720fa136d25819424
jaketaussig <jaketaussig@gmail.com>
2025-06-02 21:04:32 -0700

    made code more concise

--- scripts/battle_participant.gd
+++ scripts/battle_participant.gd
@@ -205,8 +205,7 @@ func enact_consume_on_self():
 	var hp_to_siphen = int(monster.max_hp * 0.05)
 	# Can only consume as much HP is missing/available. 
 	var benefactor_max_hp_to_siphen = consume_benefactor.monster.max_hp - consume_benefactor.monster.hp
-	var target_max_hp_to_siphen = monster.hp
-	var max_hp_to_siphen = min(benefactor_max_hp_to_siphen, target_max_hp_to_siphen)
+	var max_hp_to_siphen = min(benefactor_max_hp_to_siphen, monster.hp)
 	if max_hp_to_siphen == 0:
 		return "%s cannot consume because they are at full HP!" % consume_benefactor.monster.character_name
 	elif max_hp_to_siphen < hp_to_siphen:
