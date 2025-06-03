65296cbeeffe7873c2882ed5543672c661bab2d9
jaketaussig <jaketaussig@gmail.com>
2025-06-02 22:12:58 -0700

    saving beginning of implementation

--- scripts/states/IncrementTurn.gd
+++ scripts/states/IncrementTurn.gd
@@ -4,7 +4,9 @@ func enter(_messages: Array = []):
 	if _is_battle_over():
 		battle.transition_state_to(battle.STATE_GAME_END)
 		return
-
+	elif _is_current_enemy_defeated():
+		pass
+		
 	battle.turn_order_index = (battle.turn_order_index + 1) % battle.battle_participants.size()
 	if battle.turn_order_index == 0:
 		battle.turn += 1
@@ -18,7 +20,11 @@ func enter(_messages: Array = []):
 		battle.transition_state_to(battle.STATE_ENEMY_ATTACK)
 
 func _is_battle_over() -> bool:
-	return battle.enemies[0].monster.hp <= 0 || battle.get_node("%Player").monster.hp <= 0
+	#TODO: Add some condition here which checks if all enemies have been defeated. 
+	return battle.get_node("%Player").monster.hp <= 0
+
+func _is_current_enemy_defeated():
+	pass
 
 func _log_turn_info():
 	print("---------------------------")
