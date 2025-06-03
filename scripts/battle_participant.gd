6aab0f98d05be3c88d71876f353c0a9b274f8c33
Ben Taussig <btaussig97@gmail.com>
2025-06-02 21:27:03 -0400

    update battle_participant.gd to refer to monster's stats

--- scripts/battle_participant.gd
+++ scripts/battle_participant.gd
@@ -146,74 +146,74 @@ func recover_from_status_effect() -> String:
 # Only called on first turn of cripple
 func enact_cripple_on_self():
 	if status_effect_turn_counter == 0:
-		atk *= 0.8
-		sp_atk *= 0.8
-		def *= 0.8
-		sp_def *= 0.8
-		speed *= 0.8
-		luck *= 0.8
-		return "All of %s's stats were lowered by 20" % character_name + "%!"
+		monster.atk *= 0.8
+		monster.sp_atk *= 0.8
+		monster.def *= 0.8
+		monster.sp_def *= 0.8
+		monster.speed *= 0.8
+		monster.luck *= 0.8
+		return "All of %s's stats were lowered by 20" % monster.character_name + "%!"
 	return ""
 
 func _recover_from_cripple():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	atk *= 1.25
-	sp_atk *= 1.25
-	def *= 1.25
-	sp_def *= 1.25
-	speed *= 1.25
-	luck *= 1.25
-	return "%s recovered from cripple and their stats were restored!" % character_name
+	monster.atk *= 1.25
+	monster.sp_atk *= 1.25
+	monster.def *= 1.25
+	monster.sp_def *= 1.25
+	monster.speed *= 1.25
+	monster.luck *= 1.25
+	return "%s recovered from cripple and their stats were restored!" % monster.character_name
 	
 func enact_burn_on_self():
 	if status_effect_turn_counter == 0:
-		atk *= 0.5
-		return "%s's attack was lowered by 50" % character_name + "%!"
+		monster.atk *= 0.5
+		return "%s's attack was lowered by 50" % monster.character_name + "%!"
 	else:
-		var hp_to_lose = int(max_hp * 0.04)
-		hp -= hp_to_lose
-		return "%s took %s damage from burn!" % [character_name, str(hp_to_lose)]
+		var hp_to_lose = int(monster.max_hp * 0.04)
+		monster.hp -= hp_to_lose
+		return "%s took %s damage from burn!" % [monster.character_name, str(hp_to_lose)]
 	
 func _recover_from_burn():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	atk *= 1.5
-	return "%s recovered from burn!" % character_name
+	monster.atk *= 1.5
+	return "%s recovered from burn!" % monster.character_name
 	
 func _recover_from_whirlpool():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	return "%s recovered from whirlpool!" % character_name
+	return "%s recovered from whirlpool!" % monster.character_name
 
 func enact_poison_on_self():
-	var hp_to_lose = int(max_hp * 0.08)
-	hp -= hp_to_lose
-	return "%s is poisoned and took %s damage!" % [character_name, hp_to_lose]
+	var hp_to_lose = int(monster.max_hp * 0.08)
+	monster.hp -= hp_to_lose
+	return "%s is poisoned and took %s damage!" % [monster.character_name, hp_to_lose]
 	
 func _recover_from_poison():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	return "%s recovered from poison" % character_name
+	return "%s recovered from poison" % monster.character_name
 	
 func _recover_from_paralyze():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	return "%s recovered from paralyze" % character_name
+	return "%s recovered from paralyze" % monster.character_name
 	
 func enact_consume_on_self():
-	var hp_to_siphen = max_hp * 0.04
-	hp -= hp_to_siphen
-	consume_benefactor.hp += hp_to_siphen
-	return "%s consumed %s HP from %s!" % [consume_benefactor.character_name, str(hp_to_siphen), character_name]
+	var hp_to_siphen = monster.max_hp * 0.04
+	monster.hp -= hp_to_siphen
+	consume_benefactor.monster.hp += hp_to_siphen
+	return "%s consumed %s HP from %s!" % [consume_benefactor.monster.character_name, str(hp_to_siphen), monster.character_name]
 	
 func _recover_from_consume():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
 	consume_benefactor = null
-	return "%s recovered from consume!" % character_name
+	return "%s recovered from consume!" % monster.character_name
 	
 func _recover_from_blind():
 	status_effect = MovesList.StatusEffect.NONE
 	status_effect_turn_counter = 0
-	return "%s recovered from blind!" % character_name
+	return "%s recovered from blind!" % monster.character_name
