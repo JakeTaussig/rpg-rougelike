67f4a2bafbf5dbe1ce87193b04a0c969bbcb0f61
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-03 18:06:40 -0400

    Create ~./scripts/states/SelectingItem.gd~

---
+++ scripts/states/SelectingItem.gd
@@ -0,0 +1,15 @@
+extends BaseState
+
+var last_focused_move_index = 0
+
+func enter(messages: Array = []):
+	%Items.visible = true
+	%ItemsMenu.get_child(last_focused_move_index).grab_focus()
+	_render_moves()
+
+func exit():
+	%Moves.visible = false
+
+func _render_moves():
+	# placeholder implementation
+	%ItemsMenu.get_child(0).text = "HP Restore"
