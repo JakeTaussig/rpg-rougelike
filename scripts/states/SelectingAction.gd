3e6a379f1f0abc89a0a1e0b22584051ed5c657ac
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-09 15:46:35 -0400

    Consumable Items (#26)
    * Write up implementation plan, commit state diagram from enact statuses PR
    
    * Update state diagram according to plan and generate image
    
    * Create `SELECTING_ITEM` Node in `%BattleStateMachine` (`./scenes/battle.tscn`)
    
    * Create `%Items` Node2D as a child of `%BottomBar` (`./scenes/battle.tscn`)
    
    * Create "Items" button handler in ~./scripts/states/SelectingAttack.gd~
    
    * Create ~./scripts/states/SelectingItem.gd~
    
    * Create `scripts/Backpack.gd` and dynamically populate (first) Item names
    
    * fully support HP consumable
    
    * Support `BaseState.handle_input`
    
    Allows player to return to `SELECTING_ACTION` from `SELECTING_ITEM` and
    `SELECTING_ATTACK` states
    
    * fix `BattleParticipant.is_player`
    
    * prepare for adding additional consumable types
    
    * remove Backpack, prepare `SELECTING_ITEM` for use with multiple items
    
    * add stat up consumable items
    
    * add scrolling to items menu
    
    * update main UI theme
    
    * style updates, add back buttons to `SELECTING_ITEM` and `SELECTING_ATTACK`
    
    * UI cleanup
    
    * text spacing adjustments
    
    * create status heal items
    
    * add PP restore, essentially lifted from meditation PR
    
    * removed unused variables and addressed debugger warnings
    
    * detach hardcoded `_pressed()` signals from player moves
    
    these signals are now attached to the buttons in the `_init_move_buttons()`
    method in `SelectingAttack.gd`
    
    * update state diagram
    
    * remove implementation plan
    
    * fix back button in `SELECTING_ATTACK_PP_RESTORE` state
    
    * update move names, fix overlapping text
    
    * SelectingItem.gd: create `selected_monster` variable
    
    * call `monster.recover_from_status_effect()` instead of setting to NONE
    
    ---------
    
    Co-authored-by: Ben Taussig <btaussig97@gmail.com>
    Co-authored-by: jaketaussig <jaketaussig@gmail.com>

--- scripts/states/SelectingAction.gd
+++ scripts/states/SelectingAction.gd
@@ -3,7 +3,10 @@ extends BaseState
 func enter(messages: Array = []):
 	%PlayerPrompt.visible = true
 	%Action.visible = true
-	%Action.get_child(0).grab_focus()
+	%ActionButtons.get_child(0).grab_focus()
+
+	for button: Button in %ActionButtons.get_children():
+		button.mouse_entered.connect(button.grab_focus)
 
 	if messages.size() == 0:
 		%PlayerPrompt.text = "What will %s do?" % %Player.selected_monster.character_name
@@ -11,8 +14,14 @@ func enter(messages: Array = []):
 		%PlayerPrompt.text = messages[0]
 
 func exit():
+	for button: Button in %ActionButtons.get_children():
+		button.mouse_entered.disconnect(button.grab_focus)
+
 	%PlayerPrompt.visible = false
 	%Action.visible = false
 
 func _on_attack_pressed() -> void:
 	battle.transition_state_to(battle.STATE_SELECTING_ATTACK)
+
+func _on_item_pressed() -> void:
+	battle.transition_state_to(battle.STATE_SELECTING_ITEM)
---
+++ scripts/states/SelectingAttackPPRestore.gd
@@ -0,0 +1,70 @@
+extends SelectingAttack
+
+func _render_moves():
+	for i in %Player.selected_monster.moves.size():
+		var move = %Player.selected_monster.moves[i]
+		if move.pp == move.max_pp:
+			%MovesMenu.get_child(i).set_theme_type_variation("DisabledButton")
+		else:
+			%MovesMenu.get_child(i).set_theme_type_variation("Button")
+
+func _init_move_buttons():
+	for i in %Player.selected_monster.moves.size():
+		%MovesMenu.get_child(i).text = %Player.selected_monster.moves[i].move_name
+		%MovesMenu.get_child(i).focus_entered.connect(func(): _display_pp_info(i))
+		%MovesMenu.get_child(i).pressed.connect(func(): _restore(i))
+
+		if %Player.selected_monster.moves[i].pp == %Player.selected_monster.moves[i].max_pp:
+			%MovesMenu.get_child(i).set_theme_type_variation("DisabledButton")
+		else:
+			%MovesMenu.get_child(i).set_theme_type_variation("Button")
+		%MovesMenu.get_child(i).mouse_entered.connect(%MovesMenu.get_child(i).grab_focus)
+
+func _restore(i: int):
+	var move = %Player.selected_monster.moves[i]
+	if move.pp == move.max_pp:
+		return
+
+	var restore_amt = 3 + (randi() % 3)
+	var initial_pp = move.pp
+	var restored_pp = min(move.max_pp, initial_pp + restore_amt)
+
+	move.pp = restored_pp
+
+	battle.transition_state_to(battle.STATE_INFO, ["%s meditated." % %Player.selected_monster.character_name, ".....", ".......", ".....", "Restored PP of %s from %d to %d." % [%Player.selected_monster.moves[i].move_name, initial_pp, restored_pp]])
+
+func _display_pp_info(move_index: int) -> void:
+	#last_focused_move_index = move_index
+	var move = %Player.selected_monster.moves[move_index]
+
+	var min_updated_pp = min(move.max_pp, move.pp + 3)
+	var max_updated_pp = min(move.max_pp, move.pp + 5)
+	var min_added_pp = min_updated_pp - move.pp
+	var max_added_pp = max_updated_pp - move.pp
+
+	if move.pp == move.max_pp:
+		%PPInfo.text = "%d / %d" % [move.pp, move.max_pp]
+		%TypeInfo.text = "PP Already full"
+		%PPInfo.set_theme_type_variation("GrayTextLabel")
+		%TypeInfo.set_theme_type_variation("GrayTextLabel")
+		return
+	elif min_added_pp == max_added_pp:
+		%PPInfo.text = "%d --> %d" % [move.pp, min_updated_pp]
+		%TypeInfo.text = "Restore %d PP" % [min_added_pp]
+	else:
+		%PPInfo.text = "%d -->  %d - %d" % [move.pp, min_updated_pp, max_updated_pp]
+		%TypeInfo.text = "Restore %d-%d PP" % [min_added_pp, max_added_pp]
+
+	%PPInfo.set_theme_type_variation("NoBorderLabel")
+	%TypeInfo.set_theme_type_variation("NoBorderLabel")
+
+func _on_move_pressed(index: int) -> void:
+	var move = %Player.selected_monster.moves[index]
+	print("\tuser input:\t\tselect %s (idx %d)" % [move.move_name, index])
+	if move.pp < move.max_pp:
+		battle.transition_state_to(
+			battle.STATE_INFO, [])
+
+func handle_input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel"):
+		battle.transition_state_to(battle.STATE_SELECTING_ITEM, ["replenish"])
