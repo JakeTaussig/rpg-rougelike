14a1728af34171d324f44cd4b8f1c6bc87217b1a
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-10 18:28:26 -0400

    store items as custom resources

---
+++ scripts/item.gd
@@ -0,0 +1,137 @@
+extends Resource
+class_name Item
+
+@export var name: String
+@export var qty: int
+@export var description: String
+
+@export var callback_name: String:
+	set(_name):
+		callback_name = _name
+		_callback = Callable(self, _name)
+var _callback: Callable
+
+static func create(_name: String, _qty: int, _description: String, _callback_name: String):
+	var output = Item.new()
+	output.name = _name
+	output.qty = _qty
+	output.description = _description
+	output.callback_name = _callback_name
+	return output
+
+# returns an identical copy of the current item
+func copy() -> Item:
+	var new_item = Item.new()
+	var properties = get_property_list()
+
+	for property in properties:
+		# Only copy custom, writable properties
+		var is_custom_property = property.usage & PROPERTY_USAGE_SCRIPT_VARIABLE
+		var is_writable_property = property.usage & PROPERTY_USAGE_STORAGE
+		if is_custom_property and is_writable_property:
+			var property_name = property["name"]
+			new_item.set(property_name, get(property_name))
+
+	return new_item
+
+func consume(selected_monster, battle):
+	if qty <= 0:
+		push_error("cannot consume item %s with <= 0 qty" % name)
+		return
+	qty -= 1
+
+	_callback.call(selected_monster, battle)
+
+func _handle_hp_restore(selected_monster, battle):
+	const HP_RESTORE_AMT = 30
+	selected_monster.hp += HP_RESTORE_AMT
+	battle.transition_state_to(battle.STATE_INFO, ["%s restored 30 HP" % selected_monster.character_name])
+
+func _handle_attack_up(selected_monster, battle):
+	const ATK_UP_FACTOR = 1.1
+	var prior_atk = selected_monster.atk
+	var current_atk = int(ATK_UP_FACTOR * prior_atk)
+	selected_monster.atk = current_atk
+	battle.transition_state_to(battle.STATE_INFO, ["%s's attack increased from %d to %d" % [selected_monster.character_name, prior_atk, current_atk]])
+
+func _handle_sp_atk_up(selected_monster, battle):
+	const SP_ATK_UP_FACTOR = 1.1
+	var prior_sp_atk = selected_monster.sp_atk
+	var current_sp_atk = int(SP_ATK_UP_FACTOR * prior_sp_atk)
+	selected_monster.sp_atk = current_sp_atk
+	battle.transition_state_to(battle.STATE_INFO, ["%s's special attack increased from %d to %d" % [selected_monster.character_name, prior_sp_atk, current_sp_atk]])
+
+func _handle_def_up(selected_monster, battle):
+	const DEF_UP_FACTOR = 1.1
+	var prior_def = selected_monster.def
+	var current_def = int(DEF_UP_FACTOR * prior_def)
+	selected_monster.def = current_def
+	battle.transition_state_to(battle.STATE_INFO, ["%s's defense increased from %d to %d" % [selected_monster.character_name, prior_def, current_def]])
+
+func _handle_sp_def_up(selected_monster, battle):
+	const SP_DEF_UP_FACTOR = 1.1
+	var prior_sp_def = selected_monster.sp_def
+	var current_sp_def = int(SP_DEF_UP_FACTOR * prior_sp_def)
+	selected_monster.sp_def = current_sp_def
+	battle.transition_state_to(battle.STATE_INFO, ["%s's special defense increased from %d to %d" % [selected_monster.character_name, prior_sp_def, current_sp_def]])
+
+func _handle_speed_up(selected_monster, battle):
+	const SPEED_UP_FACTOR = 1.1
+	var prior_speed = selected_monster.speed
+	var current_speed = int(SPEED_UP_FACTOR * prior_speed)
+	selected_monster.speed = current_speed
+	battle.transition_state_to(battle.STATE_INFO, ["%s's speed increased from %d to %d" % [selected_monster.character_name, prior_speed, current_speed]])
+
+func _handle_luck_up(selected_monster, battle):
+	const LUCK_UP_FACTOR = 1.1
+	var prior_luck = selected_monster.luck
+	var current_luck = int(LUCK_UP_FACTOR * prior_luck)
+	selected_monster.luck = current_luck
+	battle.transition_state_to(battle.STATE_INFO, ["%s's luck increased from %d to %d" % [selected_monster.character_name, prior_luck, current_luck]])
+
+func _handle_burn_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.BURN:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s healed their BURN" % selected_monster.character_name])
+
+func _handle_cripple_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.CRIPPLE:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s recovered from CRIPPLE" % selected_monster.character_name])
+
+func _handle_whirlpool_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.WHIRLPOOL:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s escaped the WHIRLPOOL" % selected_monster.character_name])
+
+func _handle_poison_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.POISON:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s was cured of POISON" % selected_monster.character_name])
+
+func _handle_paralyze_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.PARALYZE:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s recovered from PARALYZE" % selected_monster.character_name])
+
+func _handle_consume_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.CONSUME:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s regained their energy" % selected_monster.character_name])
+
+func _handle_blind_heal(selected_monster, battle):
+	if selected_monster.status_effect == MovesList.StatusEffect.BLIND:
+		selected_monster.recover_from_status_effect()
+		battle.transition_state_to(battle.STATE_INFO, ["%s's vision was restored" % selected_monster.character_name])
+
+func _handle_pp_restore(selected_monster, battle):
+	var all_moves_full_pp = true
+	for move: Move in selected_monster.moves:
+		if move.pp < move.max_pp:
+			all_moves_full_pp = false
+			break
+
+	if all_moves_full_pp:
+		return
+
+	battle.transition_state_to(battle.STATE_SELECTING_ATTACK_PP_RESTORE)
--- scripts/states/SelectingItem.gd
+++ scripts/states/SelectingItem.gd
@@ -7,7 +7,7 @@ func enter(_messages: Array = []):
 	_render_items()
 	call_deferred("_refocus")
 	if _messages.size() != 0 && _messages[0] == "replenish":
-		items[last_focused_item_index].qty += 1
+		%Player.items[last_focused_item_index].qty += 1
 
 func _refocus():
 	%ItemsMenu.get_child(last_focused_item_index).grab_focus()
@@ -27,181 +27,41 @@ func handle_input(event: InputEvent):
 		battle.transition_state_to(battle.STATE_SELECTING_ACTION)
 
 func _render_items():
-	# placeholder implementation
-	for i in items.size():
+	# Clear existing items first (except the first one which has focus signals)
+	var child_count = %ItemsMenu.get_child_count()
+	while child_count > 0:
+		var second_child = %ItemsMenu.get_child(0)
+		second_child.free()
+		child_count = %ItemsMenu.get_child_count()
+
+	var visible_item_index = 0
+	for i in %Player.items.size():
+		if %Player.items[i].qty <= 0:
+			continue  # Skip items with qty <= 0
+
 		var menu_button
-		if i < %ItemsMenu.get_children().size():
-			menu_button = %ItemsMenu.get_child(i)
+		if visible_item_index < %ItemsMenu.get_child_count():
+			menu_button = %ItemsMenu.get_child(visible_item_index)
 		else:
 			# duplicate the second menu button, because the first has focus signals connected to the back button
-			menu_button = %ItemsMenu.get_child(1).duplicate()
+			menu_button = %ItemsMenu.get_child(0).duplicate()
 			%ItemsMenu.add_child(menu_button)
 
-		menu_button.text = items[i].name
+		menu_button.text = %Player.items[i].name
 		menu_button.focus_entered.connect(func(): _display_qty_info(i))
 		menu_button.mouse_entered.connect(menu_button.grab_focus)
 		menu_button.pressed.connect(func(): _on_item_pressed(i))
+		menu_button.set_theme_type_variation("Button")
 
-		if items[i].qty <= 0:
-			menu_button.set_theme_type_variation("DisabledButton")
-		else:
-			menu_button.set_theme_type_variation("Button")
-
+		visible_item_index += 1
 # callback used when a item is focused
 # displays the PP and type of the item in $"items/ItemsInfo"
 func _display_qty_info(item_index: int) -> void:
 	last_focused_item_index = item_index
 
-	%ItemQtyInfo.text = "%d / 99" % items[item_index].qty
-	%ItemTypeInfo.text = items[item_index].description
+	%ItemQtyInfo.text = "%d / 99" % %Player.items[item_index].qty
+	%ItemTypeInfo.text = %Player.items[item_index].description
 
 func _on_item_pressed(item_index: int) -> void:
-	var item = items[item_index]
-	if item.qty > 0:
-		item.callback.call()
-		item.qty -= 1
-
-var items: Array[Item] = []
-
-func _ready():
-	var selected_monster = %Player.selected_monster
-
-	var hp_restore = Item.create("HP restore", 5, "Heals 30 HP", func():
-		const HP_RESTORE_AMT = 30
-		selected_monster.hp += HP_RESTORE_AMT
-		battle.transition_state_to(battle.STATE_INFO, ["%s restored 30 HP" % selected_monster.character_name])
-		)
-
-	var attack_up = Item.create("Attack Up", 5, "Boosts attack by 10%", func():
-		const ATK_UP_FACTOR = 1.1
-		var prior_atk = selected_monster.atk
-		var current_atk = int(ATK_UP_FACTOR * prior_atk)
-		selected_monster.atk = current_atk
-		battle.transition_state_to(battle.STATE_INFO, ["%s's attack increased from %d to %d" % [selected_monster.character_name, prior_atk, current_atk]])
-		)
-
-	var sp_atk_up = Item.create("Sp. Atk Up", 5, "Boosts special attack by 10%", func():
-		const SP_ATK_UP_FACTOR = 1.1
-		var prior_sp_atk = selected_monster.sp_atk
-		var current_sp_atk = int(SP_ATK_UP_FACTOR * prior_sp_atk)
-		selected_monster.sp_atk = current_sp_atk
-		battle.transition_state_to(battle.STATE_INFO, ["%s's special attack increased from %d to %d" % [selected_monster.character_name, prior_sp_atk, current_sp_atk]])
-		)
-
-	var def_up = Item.create("Def. Up", 5, "Boosts def. by 10%", func():
-		const DEF_UP_FACTOR = 1.1
-		var prior_def = selected_monster.def
-		var current_def = int(DEF_UP_FACTOR * prior_def)
-		selected_monster.def = current_def
-		battle.transition_state_to(battle.STATE_INFO, ["%s's defense increased from %d to %d" % [selected_monster.character_name, prior_def, current_def]])
-		)
-
-	var sp_def_up = Item.create("Sp. Def Up", 5, "Boosts special def. by 10%", func():
-		const SP_DEF_UP_FACTOR = 1.1
-		var prior_sp_def = selected_monster.sp_def
-		var current_sp_def = int(SP_DEF_UP_FACTOR * prior_sp_def)
-		selected_monster.sp_def = current_sp_def
-		battle.transition_state_to(battle.STATE_INFO, ["%s's special defense increased from %d to %d" % [selected_monster.character_name, prior_sp_def, current_sp_def]])
-		)
-
-	var speed_up = Item.create("Speed Up", 5, "Boosts speed by 10%", func():
-		const SPEED_UP_FACTOR = 1.1
-		var prior_speed = selected_monster.speed
-		var current_speed = int(SPEED_UP_FACTOR * prior_speed)
-		selected_monster.speed = current_speed
-		battle.transition_state_to(battle.STATE_INFO, ["%s's speed increased from %d to %d" % [selected_monster.character_name, prior_speed, current_speed]])
-		)
-
-	var luck_up = Item.create("Luck Up", 5, "Boosts luck by 10%", func():
-		const LUCK_UP_FACTOR = 1.1
-		var prior_luck = selected_monster.luck
-		var current_luck = int(LUCK_UP_FACTOR * prior_luck)
-		selected_monster.luck = current_luck
-		battle.transition_state_to(battle.STATE_INFO, ["%s's luck increased from %d to %d" % [selected_monster.character_name, prior_luck, current_luck]])
-		)
-
-	var burn_heal = Item.create("Aloe Vera", 8, "Heals player from BURN", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.BURN:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s healed their BURN" % selected_monster.character_name])
-		)
-
-	var cripple_heal = Item.create("Pain Meds", 8, "Heals player from CRIPPLE", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.CRIPPLE:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s recovered from CRIPPLE" % selected_monster.character_name])
-		)
-
-	var whirlpool_heal = Item.create("Lifeguard", 8, "Save plyr. from WHIRLPOOL", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.WHIRLPOOL:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s escaped the WHIRLPOOL" % selected_monster.character_name])
-		)
-
-	var poison_heal = Item.create("Antidote", 8, "Cures player from POISON", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.POISON:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s was cured of POISON" % selected_monster.character_name])
-		)
-
-	var paralyze_heal = Item.create("Paralyze Heal", 8, "Heals player from PARALYZE", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.PARALYZE:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s recovered from PARALYZE" % selected_monster.character_name])
-		)
-
-	var consume_heal = Item.create("Antibiotics", 8, "Frees player from CONSUME", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.CONSUME:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s regained their energy" % selected_monster.character_name])
-		)
-
-	var blind_heal = Item.create("Eye Drops", 8, "Cures player from BLIND", func():
-		if selected_monster.status_effect == MovesList.StatusEffect.BLIND:
-			selected_monster.recover_from_status_effect()
-			battle.transition_state_to(battle.STATE_INFO, ["%s's vision was restored" % selected_monster.character_name])
-		)
-
-	var pp_restore = Item.create("PP Restore", 8, "Restore a move's PP", func():
-		var all_moves_full_pp = true
-		for move: Move in selected_monster.moves:
-			if move.pp < move.max_pp:
-				all_moves_full_pp = false
-				break
-
-		if all_moves_full_pp:
-			return
-
-		battle.transition_state_to(battle.STATE_SELECTING_ATTACK_PP_RESTORE)
-		)
-	items = [
-		hp_restore,
-		attack_up,
-		sp_atk_up,
-		def_up,
-		sp_def_up,
-		speed_up,
-		luck_up,
-		burn_heal,
-		cripple_heal,
-		whirlpool_heal,
-		poison_heal,
-		paralyze_heal,
-		consume_heal,
-		blind_heal,
-		pp_restore
-	]
-
-class Item:
-	var name: String
-	var qty: int
-	var description: String
-	var callback: Callable
-
-	static func create(_name: String, _qty: int, _description: String, _callback: Callable):
-		var output = Item.new()
-		output.name = _name
-		output.qty = _qty
-		output.description = _description
-		output.callback = _callback
-		return output
+	var item = %Player.items[item_index]
+	item.consume(%Player.selected_monster, battle)
