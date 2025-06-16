4dad166c0c551985df324e1f17e47642048f9d7f
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-16 18:47:21 -0400

    UI: Color Code types and status effects on health panels (#37)
    * color code battle participant type on health panel
    
    * set status effect to appropriate color
    
    * minor updates
    
    * `*_to_color`: set default color to white, add test scene
    
    * set human color to YELLOW
    
    ---------
    
    Co-authored-by: Ben Taussig <btaussig97@gmail.com>

--- scripts/moves_list.gd
+++ scripts/moves_list.gd
@@ -29,3 +29,42 @@ const TYPE_CHART = [
 [ 1.0,  2.0,  1.0,  2.0,  0.5,  1.0,  0.5 ], # DARK
 [ 1.0,  0.5,  2.0,  1.0,  0.5,  2.0,  1.0 ]  # LIGHT
 ]
+
+
+static func type_to_color(type: Type) -> Color:
+	match type:
+		Type.HUMAN:
+			return Color(0.996, 0.906, 0.38, 1.0)
+		Type.FIRE:
+			return Color(0.894, 0.231, 0.267, 1.0)
+		Type.WATER:
+			return Color(0.0, 0.6, 0.859, 1.0)
+		Type.PLANT:
+			return Color(0.388, 0.78, 0.302, 1.0)
+		Type.PLASMA:
+			return Color(0.71, 0.314, 0.533, 1.0)
+		Type.DARK:
+			return Color(0.227, 0.267, 0.4, 1.0)
+		Type.LIGHT:
+			return Color(0.545, 0.608, 0.706, 1.0)
+
+	return Color(1.0, 1.0, 1.0, 1.0)
+
+static func status_effect_to_color(status_effect: StatusEffect) -> Color:
+	match status_effect:
+		MovesList.StatusEffect.CRIPPLE:
+			return Color(0.996, 0.906, 0.38, 1.0)
+		MovesList.StatusEffect.BURN:
+			return Color(0.894, 0.231, 0.267, 1.0)
+		MovesList.StatusEffect.WHIRLPOOL:
+			return Color(0.0, 0.6, 0.859, 1.0)
+		MovesList.StatusEffect.POISON:
+			return Color(0.388, 0.78, 0.302, 1.0)
+		MovesList.StatusEffect.PARALYZE:
+			return Color(0.71, 0.314, 0.533, 1.0)
+		MovesList.StatusEffect.CONSUME:
+			return Color(0.227, 0.267, 0.4, 1.0)
+		MovesList.StatusEffect.BLIND:
+			return Color(0.545, 0.608, 0.706, 1.0)
+
+	return Color(1.0, 1.0, 1.0, 1.0)
