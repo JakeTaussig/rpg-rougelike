eeb66b99b3f80a752a5b653b066ce195a476d4cf
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-17 16:31:38 -0400

    Add outlines and particles to battle participant to indicate status effects (#34)
    * initial implementation: create status_emitter.tscn and add to `BattleParticipant`
    
    * add test scene, add color-coded outlines to particles and battle partipant
    
    * update particle shader params
    
    * layout adjustments
    
    * rename `_render_battler` --> `render_battler`
    
    * update particles to match existing color coding
    
    * add clarifying comments when setting outlines
    
    * credit original author of outline shader

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -66,6 +66,10 @@ func render_hp(player_monster, enemy_monster):
 	
 	call_deferred("_adjust_player_health_panel_position")
 
+func render_battlers():
+	GameManager.player.render_battler()
+	GameManager.enemy.render_battler()
+
 func _adjust_player_health_panel_position():
 	player_health_panel.adjust_position()
 
