a0e47f013f3872120bc7df034398bcc04fd38493
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-06-23 13:30:32 -0400

    add PP up powerup

---
+++ resources/trinket_strategies/pp_up_strategy.gd
@@ -0,0 +1,7 @@
+class_name PPUpTrinketStrategy
+extends TrinketStrategy
+
+func ApplyEffect(monster: Monster):
+	for move in monster.moves:
+		move.max_pp = int(1.25 * move.max_pp)
+		move.pp = move.max_pp
