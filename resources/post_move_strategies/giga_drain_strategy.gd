2cd6f382b2984821bd788369ba225ec0d4864f4c
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-07-03 16:48:38 -0400

    Update Giga Drain message when attacker's HP is full

--- resources/post_move_strategies/giga_drain_strategy.gd
+++ resources/post_move_strategies/giga_drain_strategy.gd
@@ -13,5 +13,7 @@ func ApplyEffect(AttackResults: Monster.AttackResults) -> String:
 
 	var gained_hp = final_hp - initial_hp
 
-	# TODO: This message appears even if 0 HP is drained -- validate that this matches what happens in pkmn
-	return "%s drained %d HP" % [AttackResults.attacker.character_name, gained_hp]
+	if gained_hp > 0:
+		return "%s drained %d HP." % [AttackResults.attacker.character_name, gained_hp]
+	else:
+		return "%s's HP is full.\nGiga Drain did not drain any HP." % AttackResults.attacker.character_name
