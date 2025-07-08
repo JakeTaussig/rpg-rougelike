0e0823c53c0bed73e19bb24386600e67ae5d4da6
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-07-08 18:13:17 -0400

    support move selection from debug menu

--- scripts/debug_menu.gd
+++ scripts/debug_menu.gd
@@ -18,7 +18,26 @@ func toggle_pause():
 	# TODO: manage focus
 
 func _render_moves_list():
+	%MovesList.get_child(0).queue_free()
+
 	for move in GameManager.moves_list.moves:
 		var button = Button.new()
 		button.text = move.move_name
 		%MovesList.add_child(button)
+
+		# TODO: display addtl. info when move is hovered
+		#button.focus_entered.connect(func(): _display_pp_info(i))
+		button.mouse_entered.connect(button.grab_focus)
+		button.pressed.connect(func(): _on_debug_move_pressed(move))
+
+
+func _on_debug_move_pressed(move: Move):
+	print("\tuser input (DEBUG):\t\tselect DEBUG move %s" % [move.move_name])
+	if move.pp > 0:
+		var attackCommand = AttackState.AttackCommand.new()
+		attackCommand.attacker = GameManager.player.selected_monster
+		attackCommand.move_index = -1
+		attackCommand._debug_move = move
+		attackCommand.target = GameManager.enemy.selected_monster
+		GameManager.current_battle.transition_state_to(GameManager.current_battle.STATE_ATTACK, [attackCommand])
+		toggle_pause()
