89c70bd0393cccda20ce545ed0395027a4d1ef95
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-07-08 18:09:08 -0400

    minor UI adjustments

--- scripts/debug_menu.gd
+++ scripts/debug_menu.gd
@@ -4,6 +4,8 @@ func _ready() -> void:
 	if !get_parent().PROCESS_MODE_PAUSABLE:
 		push_warning("warning: parent is not pausable")
 
+	_render_moves_list()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		toggle_pause()
@@ -14,3 +16,9 @@ func toggle_pause():
 	visible = paused
 
 	# TODO: manage focus
+
+func _render_moves_list():
+	for move in GameManager.moves_list.moves:
+		var button = Button.new()
+		button.text = move.move_name
+		%MovesList.add_child(button)
