abd700abf5e071f5e553ccaf4c246b76b1787c2a
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-07-09 15:06:37 -0400

    use `Tree` node to represent moves as a table

--- scripts/debug_menu.gd
+++ scripts/debug_menu.gd
@@ -18,20 +18,30 @@ func toggle_pause():
 	# TODO: manage focus
 
 func _render_moves_list():
-	%MovesList.get_child(0).queue_free()
-
-	var alphabetized_moves_list = GameManager.moves_list.moves.duplicate()
+	var alphabetized_moves_list: Array[Move] = GameManager.moves_list.moves.duplicate()
 	alphabetized_moves_list.sort_custom(_sort_moves_alphabetically)
 
+	%MovesList.create_item()
+	%MovesList.set_column_title(0, "name")
+	%MovesList.set_column_title(1, "type")
+	%MovesList.set_column_title(2, "pwr")
+	%MovesList.set_column_title(3, "acc")
+
+	%MovesList.set_column_expand(0, true)
+	%MovesList.set_column_expand_ratio(0, 2)
+
 	for move in alphabetized_moves_list:
-		var button = Button.new()
-		button.text = move.move_name
-		%MovesList.add_child(button)
+		var moveItem: TreeItem = %MovesList.create_item()
+		moveItem.set_text(0, move.move_name)
+		moveItem.set_text(1, MovesList.Type.keys()[move.type])
+		moveItem.set_custom_color(1, MovesList.type_to_color(move.type))
+		moveItem.set_text(2, str(move.base_power))
+		moveItem.set_text(3, str(move.acc))
 
 		# TODO: display addtl. info when move is hovered
 		#button.focus_entered.connect(func(): _display_pp_info(i))
-		button.mouse_entered.connect(button.grab_focus)
-		button.pressed.connect(func(): _on_debug_move_pressed(move))
+		#nameItem.mouse_entered.connect(nameItem)
+		#nameItem.pressed.connect(func(): _on_debug_move_pressed(move))
 
 func _sort_moves_alphabetically(a: Move, b: Move) -> bool:
 	if a.move_name < b.move_name:
