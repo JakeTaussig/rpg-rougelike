05905b46b36328532575b3796c95725c2c467470
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-07-14 14:34:16 -0400

    pause when Q or P is pressed

--- scripts/debug_menu.gd
+++ scripts/debug_menu.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	_render_trinkets_list()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_pause"):
 		toggle_pause()
 
 func toggle_pause():
