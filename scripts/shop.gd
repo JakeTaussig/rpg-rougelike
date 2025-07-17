706bfdf70487c7210f61987220155c7f92216303
Ben Taussig <btaussig97@gmail.com>
2025-07-17 11:05:03 -0400

    straighten up `scripts/shop.gd`; add comments to longer functions

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -4,16 +4,36 @@ class_name Shop
 signal shop_ended
 
 # The shop will contain this many trinkets
-const N_TRINKETS = 3
+const N_TRINKETS: int = 3
+
+# TODO: introduce varying trinket costs
+# For now, every trinket will cost this much
+const TRINKET_COST: int = 165
+
+const DISABLED_GRAY: Color = Color(0.545, 0.608, 0.706, 1.0)
+const WHITE: Color = Color(1.0, 1.0, 1.0, 1.0)
 
 var trinkets: Array[Trinket] = []
 var purchased_trinkets: Array[bool] = []
+
 
 func setup():
 	_roll_trinkets()
 	_init_trinket_menu_buttons()
 	_render_trinkets()
 	_render_player_money()
+
+
+func _roll_trinkets():
+	while trinkets.size() < N_TRINKETS:
+		var trinket = GameManager.trinkets_list.trinkets.pick_random()
+
+		# TODO: validate that the player does not have the trinket before adding it to the shop
+		# TODO: consider preventing trinkets from appearing in the shop more than once
+		# TODO: need a fallback if less than N_TRINKETS trinkets are available
+		if !trinkets.has(trinket):
+			trinkets.append(trinket)
+			purchased_trinkets.append(false)
 
 
 func _init_trinket_menu_buttons():
@@ -27,52 +47,45 @@ func _init_trinket_menu_buttons():
 		trinket_button.connect("mouse_exited", _on_trinket_focus_exit)
 		trinket_button.pressed.connect(func(): _on_trinket_button_pressed(i))
 
-func _roll_trinkets():
-	while trinkets.size() < N_TRINKETS:
-		var trinket = GameManager.trinkets_list.trinkets.pick_random()
-
-		# TODO: validate that the player does not have the trinket before adding it to the shop
-		# TODO: consider preventing trinkets from appearing in the shop more than once
-		# TODO: need a fallback if less than N_TRINKETS trinkets are available
-		if !trinkets.has(trinket):
-			trinkets.append(trinket)
-			purchased_trinkets.append(false)
 
 func _render_trinkets():
 	for i in range(N_TRINKETS):
 		var trinket_entry: HBoxContainer = %TrinketContainer.get_child(i)
 		trinket_entry.get_node("TrinketName").text = trinkets[i].trinket_name
 		trinket_entry.get_node("TrinketIcon").texture = trinkets[i].icon
+		trinket_entry.get_node("TrinketCost").text = "¶ %d" % TRINKET_COST
 
 
 func _render_player_money():
 	%Money.text = "¶ %d" % GameManager.player.money
 
 
+# When a trinket is focused, show its information (name, icon, description,
+# cost) on the right side of the shop UI
 func _on_trinket_focus(trinket_index: int):
 	var trinket: Trinket = trinkets[trinket_index]
 
 	%TrinketInfo.text = trinket.description
-	%TrinketInfo.show()
 	%TrinketIconEnlarged.texture = trinket.icon
 	%TrinketName.text = trinket.trinket_name
-	# TODO: introduce varying trinket costs
-	var trinket_cost = 100
-	%TrinketCost.text = "¶ %d" % trinket_cost
+	%TrinketCost.text = "¶ %d" % TRINKET_COST
 
 	var trinket_cost_label = %TrinketNameAndCost/TrinketCost
+
+	# If the trinket has been purchased, show its icon in gray and cross out the cost
 	if purchased_trinkets[trinket_index]:
 		trinket_cost_label.text = "[s]%s[/s]" % trinket_cost_label.text
-		trinket_cost_label.add_theme_color_override("default_color", Color(0.545, 0.608, 0.706, 1.0))
+		trinket_cost_label.add_theme_color_override("default_color", DISABLED_GRAY)
 
 		%TrinketIconEnlarged.material = load("res://assets/shaders/grayscale-material.tres")
 	else:
-		trinket_cost_label.add_theme_color_override("default_color", Color(1.0, 1.0, 1.0, 1.0))
+		trinket_cost_label.add_theme_color_override("default_color", WHITE)
 
 	%TrinketInfo.show()
 	%TrinketIconEnlarged.show()
 	%TrinketName.show()
 	%TrinketCost.show()
+
 
 func _on_trinket_focus_exit():
 	%TrinketInfo.hide()
@@ -81,38 +94,44 @@ func _on_trinket_focus_exit():
 	%TrinketName.hide()
 	%TrinketCost.hide()
 
+
 func _on_trinket_button_pressed(trinket_index: int):
 	var trinket: Trinket = trinkets[trinket_index]
 	var trinket_button: Button = %TrinketContainer.get_child(trinket_index).get_node("TrinketName")
 	var trinket_cost_label: RichTextLabel = %TrinketContainer.get_child(trinket_index).get_node("TrinketCost")
 	var trinket_icon: TextureRect = %TrinketContainer.get_child(trinket_index).get_node("TrinketIcon")
 
-	# TODO: introduce varying trinket costs
-	var trinket_cost = 100
-
-	if GameManager.player.money < trinket_cost:
+	if GameManager.player.money < TRINKET_COST:
 		return
 
+	# when the trinket is purchased:
+	# - disable its button
+	# - draw a line through the cost
+	# - render its icon in black-and-white
 	trinket_button.disabled = true
 	trinket_cost_label.text = "[s]%s[/s]" % trinket_cost_label.text
-	trinket_cost_label.add_theme_color_override("default_color", Color(0.545, 0.608, 0.706, 1.0))
+	trinket_cost_label.add_theme_color_override("default_color", DISABLED_GRAY)
 	trinket_icon.material = load("res://assets/shaders/grayscale-material.tres")
 
+	# on the right-side summary of the trinket:
+	# - draw a line through the cost
+	# - render its icon in black-and-white
+	var summary_trinket_cost_label = %TrinketNameAndCost/TrinketCost
+	summary_trinket_cost_label.text = "[s]%s[/s]" % summary_trinket_cost_label.text
+	summary_trinket_cost_label.add_theme_color_override("default_color", DISABLED_GRAY)
+	%TrinketIconEnlarged.material = load("res://assets/shaders/grayscale-material.tres")
+
+	# charge the player for the trinket
+	GameManager.player.money -= TRINKET_COST
+	_render_player_money()
+
+	# Give the trinket to the player, apply it, and redisplay the trinket shelf
 	GameManager.player.trinkets.append(trinket)
 	trinket.strategy.ApplyEffect(GameManager.player.selected_monster)
 	GameManager.player.emit_trinkets_updated_signal()
 	%TrinketShelf.render_trinkets()
 
-	GameManager.player.money -= trinket_cost
-	_render_player_money()
-
 	purchased_trinkets[trinket_index] = true
-
-	var summary_trinket_cost_label = %TrinketNameAndCost/TrinketCost
-	summary_trinket_cost_label.text = "[s]%s[/s]" % summary_trinket_cost_label.text
-	summary_trinket_cost_label.add_theme_color_override("default_color", Color(0.545, 0.608, 0.706, 1.0))
-
-	%TrinketIconEnlarged.material = load("res://assets/shaders/grayscale-material.tres")
 
 
 func _on_exit_button_pressed() -> void:
