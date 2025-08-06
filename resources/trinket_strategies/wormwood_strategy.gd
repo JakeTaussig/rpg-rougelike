f2e6577ca3f55eea0da64b32fe38df81856a16a3
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-08-05 23:42:25 -0400

    Add new item Wormwood (Boost `WATER` type attacks) (#84)

---
+++ resources/trinket_strategies/wormwood_strategy.gd
@@ -0,0 +1,9 @@
+class_name WormwoodTrinketStrategy
+extends TrinketStrategy
+
+
+func ApplyEffect(monster: Monster):
+	for move in monster.moves:
+		if move.type == MovesList.Type.WATER:
+			move.base_power = int(float(move.base_power) * 1.25)
+		
