77c2dfd4b6cd037e8a612e8dbbf940a4d951ef0d
Ben Taussig <btaussig97@gmail.com>
2025-08-07 14:26:29 -0400

    fix bug; always respect priority moves over speed

--- scripts/states/IncrementTurn.gd
+++ scripts/states/IncrementTurn.gd
@@ -22,7 +22,7 @@ func enter(_messages: Array = []):
 		var current_attack = battle.get_sorted_attacks()[attacks_executed]
 
 		attacks_executed += 1
-		if battle.active_monsters[attacks_executed - 1] == battle.player.selected_monster:
+		if current_attack.attacker == battle.player.selected_monster:
 			battle.transition_state_to(battle.STATE_ATTACK, [current_attack])
 		else:
 			# if the enemy is alive, process its attack. otherwise, return to
