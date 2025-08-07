fb4b2ad61ef97da8c77262eb9c93afc7c426fe5e
jaketaussig <jaketaussig@gmail.com>
2025-08-07 15:56:33 -0700

    Added border to tracker

--- scripts/tracker.gd
+++ scripts/tracker.gd
@@ -2,13 +2,13 @@ extends Control
 class_name Tracker
 
 func populate_player_tracker():
-	$TrackerInfo/Character_Name.text = GameManager.player.selected_monster.character_name
-	$TrackerInfo/HP.text = "%d/%d" % [GameManager.player.selected_monster.hp, GameManager.player.selected_monster.max_hp]
-	$TrackerInfo/ATK.text = str(GameManager.player.selected_monster.atk)
-	$TrackerInfo/SP_ATK.text = str(GameManager.player.selected_monster.sp_atk)
-	$TrackerInfo/DEF.text = str(GameManager.player.selected_monster.def)
-	$TrackerInfo/SP_DEF.text = str(GameManager.player.selected_monster.sp_def)
-	$TrackerInfo/SPEED.text = str(GameManager.player.selected_monster.speed)
-	$TrackerInfo/LUCK.text = str(GameManager.player.selected_monster.luck)
-	$TrackerInfo/CRIT.text = str(GameManager.player.selected_monster.crit_chance)
-	$TrackerInfo/ACC.text = str(GameManager.player.selected_monster.acc)
+	$Panel/TrackerInfo/Character_Name.text = GameManager.player.selected_monster.character_name
+	$Panel/TrackerInfo/HP.text = "%d/%d" % [GameManager.player.selected_monster.hp, GameManager.player.selected_monster.max_hp]
+	$Panel/TrackerInfo/ATK.text = str(GameManager.player.selected_monster.atk)
+	$Panel/TrackerInfo/SP_ATK.text = str(GameManager.player.selected_monster.sp_atk)
+	$Panel/TrackerInfo/DEF.text = str(GameManager.player.selected_monster.def)
+	$Panel/TrackerInfo/SP_DEF.text = str(GameManager.player.selected_monster.sp_def)
+	$Panel/TrackerInfo/SPEED.text = str(GameManager.player.selected_monster.speed)
+	$Panel/TrackerInfo/LUCK.text = str(GameManager.player.selected_monster.luck)
+	$Panel/TrackerInfo/CRIT.text = str(GameManager.player.selected_monster.crit_chance)
+	$Panel/TrackerInfo/ACC.text = str(GameManager.player.selected_monster.acc)
