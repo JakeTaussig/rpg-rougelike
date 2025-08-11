89a20bbeb22b1b511681e8ba35dddd31c10e0bc5
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-08-11 17:54:07 -0400

    Add consumables to shop (#104)
    * WIP: Add consumables to shop
    
    * update shop layout
    
    * add option to render prana in tracker title, charge player for consumables
    
    * `Battle.tscn`: extract moves_list to its own scene
    
    * move battle-specific `on_move_pressed` function back to SelectingAttack.gd
    
    * extract check for whether move is disabled into its own Callable
    
    * add Moves menu to shop
    
    * fix text expansion issues in tracker
    
    * ux fixes when restoring move PP
    
    * rename moves_list.tscn --> moves_menu.tscn
    
    * move moves_menu.gd to `scripts/` directory
    
    * fix centering in tracker
    
    * code review
    
    * prevent the user from purchasing a PP restore for a move with full PP
    
    ---------
    
    Co-authored-by: Ben Taussig <btaussig97@gmail.com>

--- scripts/tracker.gd
+++ scripts/tracker.gd
@@ -3,9 +3,11 @@ class_name Tracker
 
 @export var is_player := true
 
+@export var show_funds := false
+
 
 func populate_player_tracker():
-	$Panel/TrackerInfo/Placeholder/Character_Name.text = GameManager.player.selected_monster.character_name
+	$Panel/TitleContainer/CharacterName.text = GameManager.player.selected_monster.character_name
 	$Panel/TrackerInfo/HP.text = "%d/%d" % [GameManager.player.selected_monster.hp, GameManager.player.selected_monster.max_hp]
 	$Panel/TrackerInfo/ATK.text = str(GameManager.player.selected_monster.atk)
 	$Panel/TrackerInfo/SP_ATK.text = str(GameManager.player.selected_monster.sp_atk)
@@ -15,7 +17,20 @@ func populate_player_tracker():
 	$Panel/TrackerInfo/LUCK.text = str(GameManager.player.selected_monster.luck)
 	$Panel/TrackerInfo/CRIT.text ="%2.0f" % (100.0 * GameManager.player.selected_monster.crit_chance) + "% " + "(%d)" % GameManager.player.selected_monster.crit_checks
 	$Panel/TrackerInfo/ACC.text = str(GameManager.player.selected_monster.acc)
+	_render_title_and_funds()
 
 
 func populate_enemy_tracker():
-	$Panel/TrackerInfo/Placeholder/Character_Name.text = GameManager.enemy.selected_monster.character_name
+	$Panel/TitleContainer/CharacterName.text = GameManager.enemy.selected_monster.character_name
+	_render_title_and_funds()
+
+
+func _render_title_and_funds():
+	if show_funds:
+		%Funds.show()
+		%Funds.text = "¶ %d" % GameManager.player.prana
+		%CharacterName.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_LEFT
+
+	else:
+		%Funds.hide()
+		%CharacterName.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_CENTER
