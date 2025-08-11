db8c5db83412ba4f1515a34daa6c9bbeb0ba2037
Ben Taussig <btaussig97@gmail.com>
2025-08-11 11:48:39 -0400

    `Battle.tscn`: extract moves_list to its own scene

---
+++ scenes/moves.gd
@@ -0,0 +1,118 @@
+extends Control
+
+@onready var moves_menu = %MovesMenu
+
+var last_focused_move_index: int = 0
+
+func _ready():
+	enter()
+
+func enter():
+	var move_buttons = moves_menu.get_children()
+	for i in GameManager.player.selected_monster.moves.size():
+		var move = GameManager.player.selected_monster.moves[i]
+
+		move_buttons[i].text = move.move_name
+		move_buttons[i].focus_entered.connect(func(): _display_pp_info(i))
+		move_buttons[i].mouse_entered.connect(move_buttons[i].grab_focus)
+		move_buttons[i].pressed.connect(func(): _on_move_pressed(i))
+
+		if move.pp <= 0:
+			move_buttons[i].set_theme_type_variation("DisabledButton")
+		else:
+			move_buttons[i].set_theme_type_variation("Button")
+			
+	move_buttons[last_focused_move_index].grab_focus()
+
+func exit():
+	var move_buttons = moves_menu.get_children()
+	for button: Button in move_buttons:
+		for dict in button.get_signal_connection_list("pressed"):
+			button.disconnect("pressed", dict.callable)
+		for dict in button.get_signal_connection_list("focus_entered"):
+			button.disconnect("focus_entered", dict.callable)
+		for dict in button.get_signal_connection_list("mouse_entered"):
+			button.disconnect("mouse_entered", dict.callable)
+
+
+func _on_move_pressed(index: int) -> void:
+	var move = GameManager.player.selected_monster.moves[index]
+	print("\tuser input:\t\tselect %s (idx %d)" % [move.move_name, index])
+	if move.pp > 0:
+		var attackCommand = AttackState.AttackCommand.new()
+		attackCommand.attacker = GameManager.player.selected_monster
+		attackCommand.target = GameManager.enemy.selected_monster
+		attackCommand.move = GameManager.player.selected_monster.moves[index]
+		GameManager.current_battle.player_attack = attackCommand
+		GameManager.current_battle.transition_state_to(GameManager.current_battle.STATE_INCREMENT_TURN)
+
+
+# callback used when a move is focused
+# displays the PP and type of the move in $"Moves/MovesInfo"
+func _display_pp_info(move_index: int) -> void:
+	last_focused_move_index = move_index
+
+	var move = GameManager.player.selected_monster.moves[move_index]
+
+	var text = "%d / %d" % [move.pp, move.max_pp]
+	var disabled: bool = move.pp <= 0
+	set_pp_info(text, disabled)
+
+	set_type_info(move.type, disabled)
+	set_move_power(move.base_power, disabled)
+	set_move_accuracy(move.acc, disabled)
+
+
+
+func set_pp_info(text: String, disabled: bool):
+	var pp_display = %PPInfo
+	var pp_label = get_node("MoveInfo/InfoContainer/PPInfoLabel")
+	pp_display.text = text
+	if disabled:
+		pp_display.set_theme_type_variation("SmallTextRedTextLabel")
+		pp_label.set_theme_type_variation("SmallTextRedTextLabel")
+
+	else:
+		pp_display.set_theme_type_variation("TinyTextLabel")
+		pp_label.set_theme_type_variation("TinyTextLabel")
+
+
+func set_type_info(type: MovesList.Type, disabled: bool):
+	var type_info = get_node("MoveInfo/InfoContainer/TypeInfo")
+	var type_label = get_node("MoveInfo/InfoContainer/TypeInfoLabel")
+
+	var text = MovesList.Type.keys()[type]
+	type_info.text = text
+
+	if disabled:
+		type_info.set_theme_type_variation("SmallTextDisabledLabel")
+		type_label.set_theme_type_variation("SmallTextDisabledLabel")
+		type_info.remove_theme_color_override("font_color")
+	else:
+		type_info.set_theme_type_variation("TinyTextLabel")
+		type_label.set_theme_type_variation("TinyTextLabel")
+
+		var type_color = MovesList.type_to_color(type)
+		type_info.add_theme_color_override("font_color", type_color)
+
+func set_move_power(power: int, disabled: bool):
+	var power_display = get_node("MoveInfo/InfoContainer/Power")
+	var power_label = get_node("MoveInfo/InfoContainer/PowerLabel")
+	power_display.text = "%d" % power
+	if disabled:
+		power_display.set_theme_type_variation("SmallTextDisabledLabel")
+		power_label.set_theme_type_variation("SmallTextDisabledLabel")
+	else:
+		power_display.set_theme_type_variation("TinyTextLabel")
+		power_label.set_theme_type_variation("TinyTextLabel")
+
+func set_move_accuracy(acc: int, disabled: bool):
+	var acc_display = get_node("MoveInfo/InfoContainer/Acc")
+	var acc_label = get_node("MoveInfo/InfoContainer/AccLabel")
+	acc_display.text = "%d" % acc
+	if disabled:
+		acc_display.set_theme_type_variation("SmallTextDisabledLabel")
+		acc_label.set_theme_type_variation("SmallTextDisabledLabel")
+	else:
+		acc_display.set_theme_type_variation("TinyTextLabel")
+		acc_label.set_theme_type_variation("TinyTextLabel")
--- scripts/states/SelectingAttack.gd
+++ scripts/states/SelectingAttack.gd
@@ -1,71 +1,18 @@
 extends BaseState
 class_name SelectingAttack
 
-var last_focused_move_index: int = 0
-
 
 func enter(_messages: Array = []):
-	_init_move_buttons()
 	battle.ui_manager.show_moves_menu(true)
-	battle.ui_manager.get_move_buttons()[last_focused_move_index].grab_focus()
+	battle.ui_manager.moves_menu.enter()
 
 
 func exit():
 	battle.ui_manager.show_moves_menu(false)
-	for button: Button in battle.ui_manager.get_move_buttons():
-		for dict in button.get_signal_connection_list("pressed"):
-			button.disconnect("pressed", dict.callable)
-		for dict in button.get_signal_connection_list("focus_entered"):
-			button.disconnect("focus_entered", dict.callable)
-		for dict in button.get_signal_connection_list("mouse_entered"):
-			button.disconnect("mouse_entered", dict.callable)
+	battle.ui_manager.moves_menu.exit()
 
 
 func handle_input(event: InputEvent):
+	# the "Back" button in the moves list emits the "ui_cancel" event when pressed
 	if event.is_action_pressed("ui_cancel"):
 		battle.transition_state_to(battle.STATE_SELECTING_ACTION)
-
-
-func _on_move_pressed(index: int) -> void:
-	var move = GameManager.player.selected_monster.moves[index]
-	print("\tuser input:\t\tselect %s (idx %d)" % [move.move_name, index])
-	if move.pp > 0:
-		var attackCommand = AttackState.AttackCommand.new()
-		attackCommand.attacker = GameManager.player.selected_monster
-		attackCommand.target = GameManager.enemy.selected_monster
-		attackCommand.move = GameManager.player.selected_monster.moves[index]
-		battle.player_attack = attackCommand
-		battle.transition_state_to(battle.STATE_INCREMENT_TURN)
-
-
-# initialize move names and connect PP info display
-func _init_move_buttons():
-	var move_buttons = battle.ui_manager.get_move_buttons()
-	for i in GameManager.player.selected_monster.moves.size():
-		var move = GameManager.player.selected_monster.moves[i]
-
-		move_buttons[i].text = move.move_name
-		move_buttons[i].focus_entered.connect(func(): _display_pp_info(i))
-		move_buttons[i].mouse_entered.connect(move_buttons[i].grab_focus)
-		move_buttons[i].pressed.connect(func(): _on_move_pressed(i))
-
-		if move.pp <= 0:
-			move_buttons[i].set_theme_type_variation("DisabledButton")
-		else:
-			move_buttons[i].set_theme_type_variation("Button")
-
-
-# callback used when a move is focused
-# displays the PP and type of the move in $"Moves/MovesInfo"
-func _display_pp_info(move_index: int) -> void:
-	last_focused_move_index = move_index
-
-	var move = GameManager.player.selected_monster.moves[move_index]
-
-	var text = "%d / %d" % [move.pp, move.max_pp]
-	var disabled: bool = move.pp <= 0
-	battle.ui_manager.set_pp_info(text, disabled)
-
-	battle.ui_manager.set_type_info(move.type, disabled)
-	battle.ui_manager.set_move_power(move.base_power, disabled)
-	battle.ui_manager.set_move_accuracy(move.acc, disabled)
