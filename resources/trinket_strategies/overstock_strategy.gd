6a752d77a10a3942875724689de80b118f0497b1
Ben Taussig <btaussig97@gmail.com>
2025-08-14 09:40:59 -0400

    remove merge artifacts

--- resources/trinket_strategies/overstock_strategy.gd
+++ resources/trinket_strategies/overstock_strategy.gd
@@ -7,4 +7,3 @@ func ApplyEffect(monster: Monster):
 	GameManager.current_shop._roll_trinkets()
 	GameManager.current_shop.init_trinket_menu_button(GameManager.N_TRINKETS - 1)
 	GameManager.current_shop._render_trinkets()
-
